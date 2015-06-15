ec942135300106f95e5e317a8a4d7d7c07c71922
thanos <thanosv@gmail.com>
2015-06-15 15:11:29 +0000

    removed routes for now

--- mykonosbiennale/urls.py
+++ mykonosbiennale/urls.py
@@ -6,13 +6,13 @@ from django.http import HttpResponseRedirect
 #from filmfestival.rest_api import FilmView
 
 urlpatterns = patterns('',
-                       
-    url(r"^index.html$", HttpResponseRedirect('/2015-antidote/')),
+    url(r'^admin/', include(admin.site.urls)),                    
+    #url(r"^index.html$", HttpResponseRedirect('/2015-antidote/')),
     # Examples:
     #url(r"^$", TemplateView.as_view(template_name="index.html"), name="home"),
     #url(r"^2013-crises-and-paganism/$", TemplateView.as_view(template_name="2013-Crises-and-Paganism.html"), name="home"),
     #url(r"^mykonos-biennale/$", TemplateView.as_view(template_name="mykonos-biennale.html"), name="home"),
-    url(r'^admin/', include(admin.site.urls)),
+
     url(r"^filmfestival/", include("filmfestival.urls")),    
     url(r"^artfestival/", include("festival.urls")),  
 #    url(r'^api/films', FilmView.as_view(), name='films'),
@@ -21,9 +21,9 @@ urlpatterns = patterns('',
     #url(r"(?P<processor>[A-Z]+:)*(?P<width>\d*)x(?P<height>\d*)(@(?P<resolution>\d+))*(Q(?P<quality>\d+))*(?P<image_url>\/.+)", 'imagelabs.views.process'),
 
     url(r"^", include("pages.urls")), 
-   url(r"^index.html", lambda x: HttpResponseRedirect('/2015-antidote/')),
-   url(r"^2013-Crises-and-Paganism.html", lambda x: HttpResponseRedirect('/2013-crisis-and-paganism/')),
-   url(r"^mykonos_biennale.html", lambda x: HttpResponseRedirect('/mykonos-biennale/')),
+   #url(r"^index.html", lambda x: HttpResponseRedirect('/2015-antidote/')),
+   #url(r"^2013-Crises-and-Paganism.html", lambda x: HttpResponseRedirect('/2013-crisis-and-paganism/')),
+   #url(r"^mykonos_biennale.html", lambda x: HttpResponseRedirect('/mykonos-biennale/')),
 )
 # urlpatterns += patterns('',
 #         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
