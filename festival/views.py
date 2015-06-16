ac90aae58a6bb03384f3ca1c36dc20c40f22065a
thanos <thanosv@gmail.com>
2015-06-16 23:20:43 +0000

    rework of nav bar

--- festival/views.py
+++ festival/views.py
@@ -5,17 +5,37 @@ from django.http import Http404
 from django.shortcuts import render_to_response
 from django.views.generic import ListView,DetailView
 
-
 import models
 
 
+from pages.views import PageMixin
 
 
-
-class ArtistList(ListView):
+class ArtistList(PageMixin, ListView):
     queryset = models.Artist.objects.filter(visible=True).order_by('name')
-          
-class ArtistDetail(DetailView):
+    def seo(self, context):
+        return {
+            'title': 'Mykonos Biennale 2015 - Antidote Artists',
+            'description': "The list of artists prtisipating in the Mykonos Biennale Treasure Hunt",
+            'url': "/artfestival/artists", 
+        }
+        
+class ArtistDetail(PageMixin, DetailView):
     model = models.Artist
-    
-    
+
+    def breadcrumbs(self, context):
+        artist = context['object']
+        return [('/', artist.festival), ('/artfestival/artists/', artist.event), ('', artist.name )]
+
+    def seo(self, context):
+        artist = context['object']
+        title =  "Mykonos Biennale 2015 - {} - {}".format(artist.event, artist.name)
+        description = "Biennale page for {}".format(artist.name)
+        url = "/artfestival/artist/{}".format(artist.slug)
+        image = artist.headshot.url
+        return {
+            'title': title,
+            'description': description,
+            'url': url,
+             'image': image,    
+        }
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,20 @@ import models
 def page(request, slug):
     page = models.Page.objects.get(slug=slug)
     return render_to_response(page.template, {'current_page':page, 'pages': models.Page.objects.filter(visible=True)})
+
+
+
+class PageMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(PageMixin, self).get_context_data(**kwargs)
+        context['pages'] = models.Page.objects.filter(visible=True)
+        context['breadcrumbs'] = self.breadcrumbs(context)
+        context['seo'] = self.seo(context)
+        return context
+    
+    def breadcrumbs(self, context):
+        pass
+        
+    def seo(self, context):
+        raise NotImplementedError()
+       
